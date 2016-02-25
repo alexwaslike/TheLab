e0cf746a0f98d107504f920f5022b500bb643220
OrrinDrake <danbreaux@suddenlink.net>
2016-02-25 15:22:55 -0600

    Menus and more Menus
    Created a MainMenu and Credits screen with functional buttons, Need to
    go back and make a Main Menu button on the Credits screen though

---
+++ Bark/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public void PlayClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void CreditsClick()
+    {
+        SceneManager.LoadScene("Credits");
+    } 
+
+    public void ExitClick()
+    {
+        Application.Quit();
+    }
+}
