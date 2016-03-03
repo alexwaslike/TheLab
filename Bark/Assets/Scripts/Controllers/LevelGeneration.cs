7c40a04b3ecb64b5a42687f60a7abae092b43735
Alexandra Willis <awil294@lsu.edu>
2016-03-03 10:37:26 -0600

    Continuing work on ground tiling

--- Bark/Assets/Scripts/Controllers/LevelGeneration.cs
+++ Bark/Assets/Scripts/Controllers/LevelGeneration.cs
@@ -6,8 +6,8 @@ public class LevelGeneration : MonoBehaviour {
 	public GameController GameController;
 
 	public List<Vector3> LevelGrid;
-	private float _max_X = 100;
-	private float _max_Y = 100;
+	private float _max_X = 1000;
+	private float _max_Y = 500;
 	private GameObject tilePrefab;
 
 	void Start(){
@@ -16,12 +16,16 @@ public class LevelGeneration : MonoBehaviour {
 
 		LevelGrid = new List<Vector3> ();
 
-		for (int x = 0; x < _max_X; x++) {
-			for(int y = 0; y <_max_Y; y++){
+        float xSize = 3.61f;
+        float ySize = 1.82f;
+
+        for (int x = 0; x < _max_X; x++) {
+            for(int y = 0; y <_max_Y; y++){
 				Vector3 newPos = new Vector3 (x, y, 0);
-				LevelGrid.Add (newPos);
-				Instantiate (tilePrefab, newPos + new Vector3(tilePrefab.transform.lossyScale.x/2, tilePrefab.transform.lossyScale.y/2, 0), Quaternion.identity);
-			}
+				//LevelGrid.Add (newPos);
+				Instantiate (tilePrefab, new Vector3(newPos.x*xSize, newPos.y*ySize, 0), Quaternion.identity);
+                Instantiate(tilePrefab, new Vector3(newPos.x * xSize + xSize/2, newPos.y * ySize - ySize/2, 0), Quaternion.identity);
+            }
 		}
 
 	}
