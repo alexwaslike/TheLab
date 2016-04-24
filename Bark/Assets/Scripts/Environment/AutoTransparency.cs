36586d2c921c242411b85d2df50c26c604558245
Alexandra Willis <awil294@lsu.edu>
2016-04-24 00:40:56 -0500

    Added transparency when walking behind objects

---
+++ Bark/Assets/Scripts/Environment/AutoTransparency.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AutoTransparency : MonoBehaviour {
+
+    private SpriteRenderer _spriteRenderer;
+    private Color _defaultColor;
+    private Color _transparentColor;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultColor = _spriteRenderer.color;
+        _transparentColor = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, 0.3f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+            _spriteRenderer.color = _transparentColor;  
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+            _spriteRenderer.color = _defaultColor;
+    }
+	
+}
